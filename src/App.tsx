32a55f895311c75829d4f18ab40fa50f90aa291b
Gerardo Acosta <gerardo.dam.acosta@gmail.com>
2026-08-06 19:46:46 -0300

    feat(auth): agregar página /recuperar-contrasena

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { DashboardLayout } from "./layouts/DashboardLayout/DashboardLayout";
 import { Dashboard } from "./pages/Dashboard/Dashboard";
 import { Login } from "./pages/Login/Login";
 import { Registro } from "./pages/Registro/Registro";
+import { RecuperarContrasena } from "./pages/RecuperarContrasena/RecuperarContrasena";
 import { Tarjetas } from "./pages/Tarjetas/Tarjetas";
 import { Usuario } from "./pages/Usuario/Usuario";
 
@@ -17,6 +18,7 @@ function App() {
       <Route element={<GuestRoute />}>
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
+        <Route path="/recuperar-contrasena" element={<RecuperarContrasena />} />
       </Route>
       <Route element={<ProtectedRoute />}>
         {/* Cualquier ruta privada futura (ej: /transactions) va como hija acá adentro, no afuera */}
